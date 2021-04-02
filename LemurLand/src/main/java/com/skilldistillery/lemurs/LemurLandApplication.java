d262fbdd034780c590993c93a7afc54c57400e0e
Rob Roselius <roselius@skilldistillery.com>
2021-04-02 15:05:33 -0600

    Stub Spring Boot project, connect JPA project dependency

---
+++ LemurLand/src/main/java/com/skilldistillery/lemurs/LemurLandApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.lemurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemurLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemurLandApplication.class, args);
+	}
+
+}
