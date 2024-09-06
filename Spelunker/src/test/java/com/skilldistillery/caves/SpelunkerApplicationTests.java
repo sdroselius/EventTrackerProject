55179e8af992b0a1c678579245afbb4fcf83b733
Rob Roselius <roselius@skilldistillery.com>
2024-09-06 16:02:51 -0600

    Stub Spring Boot REST project, add JPA project dependency

---
+++ Spelunker/src/test/java/com/skilldistillery/caves/SpelunkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.caves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpelunkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
