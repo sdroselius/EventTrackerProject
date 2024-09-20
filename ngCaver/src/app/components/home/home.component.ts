b92f1caaf9ad7751564cbe3d99508af5f91c3496
Rob Roselius <roselius@skilldistillery.com>
2024-09-20 15:35:19 -0600

    Frame out home component, cave service, full-stack smoke test

--- ngCaver/src/app/components/home/home.component.ts
+++ ngCaver/src/app/components/home/home.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CaveService } from '../../services/cave.service';
+import { errorContext } from 'rxjs/internal/util/errorContext';
+import { Cave } from '../../models/cave';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  caves: Cave[] = [];
+
+  constructor(
+    private caveService: CaveService
+  ) {}
+
+  ngOnInit(): void {
+    this.reloadCaves();
+  }
+
+  reloadCaves(): void {
+    this.caveService.index().subscribe( {
+      next: (caveList) => {
+        this.caves = caveList;
+      },
+      error: (fail) => {
+        console.error('HomeComponent.reloadCaves: error retrieving cave list');
+        console.error(fail);
+      }
+    } );
+  }
 
 }
--- ngCaver/src/app/services/cave.service.ts
+++ ngCaver/src/app/services/cave.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Cave } from '../models/cave';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CaveService {
 
-  constructor() { }
+  url = environment.baseUrl + "api/caves";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  index(): Observable<Cave[]> {
+    return this.http.get<Cave[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('CaveService.index(): error retrieving caves: ' + err)
+        );
+      })
+    );
+  }
+
 }
